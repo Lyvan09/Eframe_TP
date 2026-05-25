f9cb0ef06876384814055af2a14fadf733af9357
Lyvan <nyalyvan2@gmail.com>
2026-05-25 21:43:22 +0100

    Finalisation, hook personnaliser et livraison

--- TaskFlow/src/pages/Dashboard.jsx
+++ TaskFlow/src/pages/Dashboard.jsx
@@ -2,30 +2,32 @@ import { useState, useEffect } from "react";
 import TaskCard from "../components/TaskCard";
 import TaskForm from "../components/TaskForm";
 
-const [tasks, setTasks] = useState(() => {
-  const donneesSauvegardees = localStorage.getItem("taskflow_data");
-  return donneesSauvegardees ? JSON.parse(donneesSauvegardees) : [];
-});
-
-useEffect(() => {
-  localStorage.setItem("taskflow_data", JSON.stringify(tasks));
-}, [tasks]);
-
-const handleAddTask = (nouvelleTache) => {
-  setTasks([...tasks, nouvelleTache]); 
-};
-
 export default function Dashboard() {
-  const [tasks, setTasks] = useState(initialTasks);
+
+  const [tasks, setTasks] = useState(() => {
+    const donneesSauvegardees = localStorage.getItem("taskflow_data");
+    return donneesSauvegardees ? JSON.parse(donneesSauvegardees) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("taskflow_data", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleAddTask = (nouvelleTache) => {
+    setTasks([...tasks, nouvelleTache]); 
+  };
 
   return (
     <div>
       <h1>TaskFlow – Dashboard</h1>
+
       <TaskForm onAddTask={handleAddTask} />
+
       <hr />
+
       <div>
         {tasks.map((task) => (
-          <TaskCard key={task.id} task={task} />
+          <TaskCard key={task.id} task={task} />  
         ))}
       </div>
     </div>
